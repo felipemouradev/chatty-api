0e8ea779fd782869a61f88ee4f34e82cbd6f34cd
felipe <felipemouracv@gmail.com>
2018-03-05 20:38:15 -0300

    Ajust message length and other fixes

--- app/models/message.js
+++ app/models/message.js
@@ -3,12 +3,18 @@ const Schema = mongoose.Schema;
 const BaseModel = require('./base_model');
 const _ = require('lodash');
 
-const messageMissedParameters = 'The request is missing required attributes or the values are invalid. For example, body exceeds max length.';
+const messageInvalidParameters = 'The request is missing required attributes or the values are invalid. For example, body exceeds max length.';
 const MessageSchema = new Schema({
-  from: {type: Schema.Types.ObjectId, ref: 'user', required: [true,messageMissedParameters]},
-  to: {type: Schema.Types.ObjectId, ref: 'user', required: [true,messageMissedParameters]},
-  description: {type: String},
-  body: {type: String, required: [true,messageMissedParameters]},
+  from: {type: Schema.Types.ObjectId, ref: 'user', required: [true, messageInvalidParameters]},
+  to: {type: Schema.Types.ObjectId, ref: 'user', required: [true, messageInvalidParameters]},
+  body: {
+    type: String,
+    required: [true, messageInvalidParameters],
+    validate: {
+      validator: (v) => v.length < 1 && v.length > 280,
+      message: messageInvalidParameters
+    }
+  },
   sentAt: {type: Date, default: new Date()},
 }, {collection: "message", versionKey: false});
 
--- app/models/user.js
+++ app/models/user.js
@@ -3,24 +3,23 @@ const Schema = mongoose.Schema;
 const BaseModel = require('./base_model');
 const _ = require('lodash');
 const TimestampPlugin = require('./plugins/timestamp');
-const HandleErrorService = require('./../services/handle-error');
 
+const messageInvalidParameters = 'The user object is bad formatted, missing attributes or has invalid values.';
 const UserSchema = new Schema({
   budget: {type: Number, default: 10},
   name: {
     type: String,
-    required: [true, 'The user object is bad formatted, missing attributes or has invalid values.']
+    required: [true, messageInvalidParameters]
   },
   username: {
     type: String,
-    required: [true, 'The user object is bad formatted, missing attributes or has invalid values.'],
+    required: [true, messageInvalidParameters],
     unique: true,
     validate: {
-    validator: function(v) {
-      return /^[a-z][a-z_\.\-0-9]*$/.test(v);
-    },
-    message: 'The user object is bad formatted, missing attributes or has invalid values.'
-  }},
+      validator: (v) => /^[a-z][a-z_\.\-0-9]*$/.test(v),
+      message: messageInvalidParameters
+    }
+  },
   createdAt: {type: Date},
   updatedAt: {type: Date}
 }, {collection: "user", versionKey: false});
