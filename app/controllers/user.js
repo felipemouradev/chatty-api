8c88a6b4c5b2cb7fcc6e847099e2b00086618596
felipe <felipemouracv@gmail.com>
2018-03-05 22:50:13 -0300

    note for budget

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -6,6 +6,17 @@ class UserController extends BaseController {
   constructor() {
     super(User);
     this.findUser = this.findUser.bind(this);
+    this.createUser = this.createUser.bind(this);
+  }
+
+  async createUser(res, req, next) {
+    return await this._save(req, res, next, ()=> {
+      // Se não for omitido o budget o usuario que conhecer o modelo
+      // poderia colocar um budget não permitido,
+      // caso a intenção seja que o novos usuarios digam qual o seu
+      // proprio buget remover o callback inteiro
+      req.body = _.omit(req.body, 'bugdet');
+    });
   }
 
   async findUser(req, res, next) {
